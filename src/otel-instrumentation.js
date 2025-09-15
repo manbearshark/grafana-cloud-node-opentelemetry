38407c173a4321fec1dc5f635be096532df4f660
manbearshark <igorski@amazon.com>
2025-09-15 13:24:58 -0700

    Added pino instrumentation and upgraded express packages to latest

--- src/otel-instrumentation.js
+++ src/otel-instrumentation.js
@@ -11,13 +11,18 @@ const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumenta
 const { LoggerProvider, BatchLogRecordProcessor } = require('@opentelemetry/sdk-logs');
 const { logs, SeverityNumber } = require('@opentelemetry/api-logs');
 const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
+const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 
 const OTEL_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT; // TODO: Provide your managed URL here
 
 // ===== GENERAL SETUP =====
 
 registerInstrumentations({
-  instrumentations: [ getNodeAutoInstrumentations() ],
+  instrumentations: [ getNodeAutoInstrumentations(
+    {
+        '@opentelemetry/instrumentation-pino': {
+        },    
+    })],
 });
 
 const resource =
@@ -43,7 +48,6 @@ const provider = new NodeTracerProvider({
 });
 
 provider.register();
-
 
 // ===== METRIC SETUP =====
 
